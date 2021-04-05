880950dcad721ff47b813efa6c585757793de8f9
OlexandrBebeh <olexandrbebeh@gmail.com>
2021-04-05 17:17:32 +0300

    Add another chrome brouser

--- src/test/java/test/test/TestKPI.java
+++ src/test/java/test/test/TestKPI.java
@@ -15,7 +15,7 @@ public class TestKPI {
 
     @Before
     public void SetUp() {
-        System.setProperty("webdriver.chrome.driver", "src/main/java/com/company/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/main/java/resources/chromedriver 7");
         driver = new ChromeDriver();
         System.out.println("Test start");
     }
